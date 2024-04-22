2212e5c59525a5465b98cbe92a573df52950f79e
Barys <128910676+Barysk@users.noreply.github.com>
2024-04-22 19:08:01 +0000

    Add files via upload

--- slstatus/config.def.h
+++ slstatus/config.def.h
@@ -65,11 +65,11 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-    { cpu_perc,     "CPU %s ",  NULL                },
-    { ram_perc,     "RAM %s ",  NULL                },
-    { keymap,       "%s ",      NULL                },
-    { battery_perc, "BAT %s ",  "BAT0"              },
-	{ datetime,     "%s",       "CE%y%M%d%H%m%s"    },
+    { cpu_perc,     " CPU %s ",  NULL                },
+    { ram_perc,     " RAM %s ",  NULL                },
+    { keymap,       " %s ",      NULL                },
+    { battery_perc, " BAT %s ",  "BAT0"              },
+	{ datetime,     " %s",       "CE%y%M%d%H%m%s"    },
 };
 
 
--- st-0.9.2/config.h
+++ st-0.9.2/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "IBM 3270:pixelsize=12:antialias=true:autohint=true";
+static char *font = "IBM 3270:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -92,6 +92,9 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
+
+/* bg opacity */
+float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -184,16 +187,16 @@ static MouseShortcut mshortcuts[] = {
 };
 
 /* Internal keyboard shortcuts. */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TERMMOD (ControlMask|ShiftMask)
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
 	{ XK_ANY_MOD,           XK_Break,       sendbreak,      {.i =  0} },
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
-    { ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-    { XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-    { TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
+	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
+	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
+	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
