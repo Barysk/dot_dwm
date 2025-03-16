668dc1812546e755e5d5ec35a2fc4f0fc01dae90
BarysK <boris.komarov@proton.me>
2025-03-16 21:12:49 +0100

    Dotfiles restructure

---
+++ dot_config/suckless/dwmblocks/blocks.def.h
@@ -0,0 +1,17 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/    /*Command*/		                                                /*Update Interval*/	/*Update Signal*/
+    {"",        "~/.config/suckless/dwmblocks/blocks_scripts/keyboard_layout",   60,                  4},
+	{"",        "~/.config/suckless/dwmblocks/blocks_scripts/volume",   0,                  10},
+// Uncomment/comment to add/remove a battery module
+    {"",        "~/.config/suckless/dwmblocks/blocks_scripts/battery",   60,                 0},
+	{"",        "~/.config/suckless/dwmblocks/blocks_scripts/time",	1,		            0},
+};
+
+// signal is being emmited withusage of kill -N $(pidof dwmblocks)
+// where N = US + 34
+// *US - Update Signal
+
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = " ";
+static unsigned int delimLen = 5;
